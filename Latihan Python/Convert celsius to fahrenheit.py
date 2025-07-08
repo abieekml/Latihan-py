ae36bb497a303a49ed368fe23f425d0793c60bd8
Abi Kamal <abii.kamal14@gmail.com>
2025-07-08 18:22:40 +0700

    Add files via upload

---
+++ Latihan Python/Convert celsius to fahrenheit.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Conversion formula: Fahrenheit = (Celcius * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} degress Celsius is equal to {fahrenheit} degress Fahrenheit")
---
+++ Latihan Python/Find are triangle.py
@@ -0,0 +1,7 @@
+# Input the base anad height from the user
+base = float(input("Enter the lenght of the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+# Calculate the are of the triangle
+area = 0.5 * base * height
+# Display the result
+print(f"The are of the triangle is: {area}")
